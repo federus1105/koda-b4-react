bb339e335c32c0a76a9ee4580983a384ce363f95
federus1105 <federusrudi@gmail.com>
2025-11-02 17:37:18 +0700

    Feat ( main ) : features redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import Router from "./routes/Routes.jsx";
+  import { StrictMode } from "react";
+  import { createRoot } from "react-dom/client";
+  import "./index.css";
+  import Router from "./routes/Routes.jsx";
+  import { Provider } from "react-redux";
+  import { persistor, reduxStore } from "./redux/store";
+  import { PersistGate } from "redux-persist/integration/react";
+  
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Router/>
-  </StrictMode>
-);
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <Provider store={reduxStore}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router />
+        </PersistGate>
+      </Provider>
+    </StrictMode>
+  );
