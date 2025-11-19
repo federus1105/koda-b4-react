f0dc1e1f0bc349f903bf179cbf5a4d23354d8aab
federus1105 <federusrudi@gmail.com>
2025-11-19 23:53:09 +0700

    Feat ( services ) : function fetch api backend login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,17 @@
 import { apiClient } from "./apiClient";
+
+
 // ===== REGISTER ======
 export const registerUser = (formData) =>
   apiClient("/auth/register", {
     method: "POST",
     body: JSON.stringify(formData),
   });
+
+// ====== LOGIN ========
+export const loginUser = async (formData) => {
+  return apiClient("/auth/login", {
+    method: "POST",
+    body: JSON.stringify(formData),
+  });
+};
