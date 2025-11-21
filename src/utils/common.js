1cbbc2eaeb98179e88dc93e718654b90a7bb2146
federus1105 <federusrudi@gmail.com>
2025-11-22 04:40:58 +0700

    Feat ( utils ) : auxiliary components needed for the order

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,6 +7,10 @@ export function formatDate(timestamp) {
   });
 }
 
+export const activeClass = "bg-orange-400 text-white";
+export const baseClass = "flex gap-5 cursor-pointer items-center p-2 rounded-xl hover:bg-orange-400 hover:text-white";
+
+// ---- CATEGORY PRODUCT ---
 export const categoryOptions = [
     { id: 1, label: "Foods", value: 1 },
     { id: 2, label: "Snack", value: 2 },
@@ -19,6 +23,20 @@ export const categoryOptions = [
     { id: 9, label: "Non Coffee", value: 9 },
 ];
 
+// --- DELIVERY ORDERS ---
+export const deliveryOptions = [
+  { id: 1, label: "Dine In"},
+  { id: 2, label: "Door Delivery"},
+  { id: 3, label: "Pick Up"},
+];
 
-  export const activeClass = "bg-orange-400 text-white";
-  export const baseClass = "flex gap-5 cursor-pointer items-center p-2 rounded-xl hover:bg-orange-400 hover:text-white";
+
+// --- PAYMENT METHOD ---
+export const paymentMethods = [
+    { id: 3, name: "Gopay", img: "https://storage.googleapis.com/flip-prod-mktg-strapi/media-library/Virtual_Account_Go_Pay_1_a96fb1aedb/Virtual_Account_Go_Pay_1_a96fb1aedb.jpg"},
+    { id: 4, name: "Paypal", img: "https://www.centerklik.com/wp-content/uploads/2016/02/paypal.jpg" },
+    { id: 5, name: "Dana", img: "https://eswpcd25uod.exactdn.com/blog/wp-content/uploads/2023/08/DANA-Apa-pun-transaksinya-selalu-ada-DANA-1.png" },
+    { id: 6, name: "BCA", img: "https://kilasjatim.com/wp-content/uploads/2024/01/BCA.png" },
+    { id: 7, name: "BRI", img: "https://cdn.ngopibareng.id/uploads/2025/2025-04-03/kapan-bank-bri-buka-jadwal-dan-jam-operasional-selama-lebaran--thumbnail-289" },
+    { id: 8, name: "OVO", img: "https://i.pinimg.com/736x/28/fd/ed/28fdedc2022b5de9ae5a7f2507eb5f2d.jpg" },
+  ];
