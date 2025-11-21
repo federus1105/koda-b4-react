389f5f0ddc4bfaf7bb3d4f6c12fa6d0289f3a019
federus1105 <federusrudi@gmail.com>
2025-11-22 04:41:27 +0700

    Feat ( services ) : function feature api call order product

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,8 +1,8 @@
 import { apiClient } from "./apiClient";
 
 
+// ---- CREATE CART ----
 export const createCart = async ({ product_id, quantity, size, variant, token }) => {
-
   const body = {
     product_id,
     quantity,
@@ -18,3 +18,30 @@ export const createCart = async ({ product_id, quantity, size, variant, token })
     body: JSON.stringify(body),
   });
 };
+
+// --- GET CART ---
+export const GetCart = async (token) => {
+  try {
+    const data = await apiClient(`/cart`, {
+      method: "GET",
+       headers: {
+         Authorization: `Bearer ${token}`,
+        }
+    });
+    return data;
+  } catch (error) {
+  console.error(error)
+  throw error;
+}
+}
+
+// --- ORDER ---
+export const OrderProduct = async (input, token)=>{
+  return await apiClient("/transactions", {
+    method: "POST",
+    headers: {
+         Authorization: `Bearer ${token}`,
+        },
+    body: JSON.stringify(input),
+  });
+}
