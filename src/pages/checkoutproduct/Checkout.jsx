4ee3eb7f970de751246dbf5c2bf104291f44c2c7
federus1105 <federusrudi@gmail.com>
2025-11-22 13:30:04 +0700

    Feat ( pages ) : feature checkout product on using api backend

--- src/pages/checkoutproduct/Checkout.jsx
+++ src/pages/checkoutproduct/Checkout.jsx
@@ -1,54 +1,123 @@
-import React, { useEffect } from "react";
-import { Plus, UserRoundPen, Mail, MapPinCheck } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Plus, PackageX, Loader2 } from "lucide-react";
 import ItemOrder from "../../components/cardproduct/ItemOrder";
 import { Link, useNavigate } from "react-router";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addToHistory,
-  resetOrder,
-  setFullname,
-  setPayment,
-  setAddress,
-  setDeliveri,
-  setTotal,
-} from "../../redux/slice/orderSlice";
+import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import { DeleteCart, GetCart, OrderProduct } from "../../services/orderService";
+import PaymentOrder from "../../components/payment/PaymentOrder";
+import { useForm } from "react-hook-form";
+import { delay, deliveryOptions } from "../../utils/common";
+import ConfirmModal from "../../components/modal/ConfirmModal";
 
 function Checkout() {
-  const selectedPayment = useSelector((state) => state.order.orderInfo.payment);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedPayment, setSelectedPayment] = useState(null);
   const user = useSelector((state) => state.auth.currentUser);
-  const product = useSelector((state) => state.order.orderInfo);
-  const { selectedProduct, pieces } = useSelector(
-    (state) => state.order.orderInfo
-  );
-  const dispatch = useDispatch();
+  const [selectedCartId, setSelectedCartId] = useState(null);
+  const token = useSelector((state) => state.auth.token);
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
+  const [isLoadingPay, setIsLoadingPay] = useState(false);
+  const [delivery, setDelivery] = useState(null);
+  const [carts, setCarts] = useState([]);
   const navigate = useNavigate();
 
-  const handlePaymentClick = (method) => {
-    dispatch(setPayment(method));
-  };
-
-  const deliveryFee = 5000;
-  const tax = 2000;
-  const itemTotal = selectedProduct?.price_discount * pieces || 0;
-  const subTotal = itemTotal + deliveryFee + tax;
-
+  // --- GET CART ---
   useEffect(() => {
-    if (user.fullname) {
-      dispatch(setFullname(user.fullname));
+    const fetchCarts = async () => {
+      try {
+        const res = await GetCart(token);
+        setCarts(res.result);
+      } catch (error) {
+        console.error(error);
+        toast.error("Terjadi kesalahan!, Silahkan coba lagi.");
+      }
+    };
+
+    fetchCarts();
+  }, [token]);
+
+
+  // --- USE FORM ---
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: user?.email || "",
+      fullname: user?.fullname || "",
+      phone: user?.phone || "",
+      address: user?.address || "",
+    },
+  });
+
+  // --- HANDLE SUBMIT ---
+  const onSubmit = async (data) => {
+    if (!selectedPayment) {
+      toast.error("Pilih metode pembayaran dulu!");
+      return;
     }
-  }, [user.fullname, dispatch]);
-
-  useEffect(() => {
-    dispatch(setTotal(subTotal));
-  }, [subTotal, dispatch]);
-
-  const handleCheckout = () => {
-    dispatch(addToHistory());
-    dispatch(resetOrder());
-    toast.success("Anda berhasil Order");
-    navigate("/history");
+    if (!delivery) {
+      toast.error("Pilih metode delivery dulu!");
+      return;
+    }
+    setIsLoadingPay(true);
+    const payload = {
+      fullname: data.fullname,
+      address: data.address,
+      phone: data.phone,
+      email: data.email,
+      id_paymentMethod: Number(selectedPayment),
+      id_delivery: Number(delivery),
+    };
+    try {
+      await Promise.all([OrderProduct(payload, token), delay(1200)]);
+      toast.success("Berhasil checkout!");
+      navigate("/history");
+    } catch (err) {
+      console.error(err);
+      toast.error("Checkout gagal, coba lagi!");
+    } finally {
+      setIsLoadingPay(false);
+    }
   };
+  
+
+  // --- HANDLE DELETE ---
+  const handleDeleteClick = (id) => {
+    setSelectedCartId(id);
+    setShowDeleteModal(true);
+  };
+
+  // --- CONFIRM DELETE ---
+  const confirmDelete = async () => {
+    setIsLoadingDelete(true);
+
+    try {
+      await Promise.all([DeleteCart(selectedCartId, token), delay(900)]);
+
+      setCarts((prev) => prev.filter((cart) => cart.id !== selectedCartId));
+      toast.success("Berhasil menghapus item!");
+    } catch (err) {
+      console.error(err);
+      toast.error("Gagal menghapus item!");
+    } finally {
+      setIsLoadingDelete(false);
+      setShowDeleteModal(false);
+      setSelectedCartId(null);
+    }
+  };
+
+  // --- LOADING ---
+
+  if (isLoadingDelete || isLoadingPay) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Loader2 className="w-16 h-16 animate-spin text-[#997950]" />
+      </div>
+    );
+  }
 
   return (
     <>
@@ -57,159 +126,146 @@ function Checkout() {
           <h1 className="font-medium text-xl lg:text-3xl">Payment Details</h1>
           <div className="flex justify-between mt-8">
             <h1 className="font-medium text-xl">Your Order</h1>
-            <button className="flex bg-orange-400 rounded-md py-2 px-4">
-              <Plus /> Add Menu
-            </button>
+            <Link to={"/product"}>
+              <button className="cursor-pointer flex bg-[#997950] rounded-md py-2 px-4">
+                <Plus /> Add Menu
+              </button>
+            </Link>
           </div>
           {/* Item Order */}
           <div className="my-5 flex flex-col gap-5">
-            <ItemOrder />
+            {carts.length === 0 ? (
+              <div className="flex flex-col items-center justify-center min-h-[400px] text-center px-4">
+                <PackageX className="w-20 h-20 text-gray-400 mb-4" />
+                <h3 className="text-2xl font-bold text-gray-700 mb-2">
+                  Product Not Found
+                </h3>
+              </div>
+            ) : (
+              carts.map((item) => (
+                <ItemOrder
+                  key={item.id}
+                  item={item}
+                  onDelete={() => handleDeleteClick(item.id)}
+                />
+              ))
+            )}
           </div>
           {/* Form */}
           <div>
-            <form className="flex flex-col gap-4">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-4"
+            >
               <h1 className="font-medium">Payment & Info Delivery</h1>
+
+              {/* Email */}
               <div className="flex flex-col gap-2">
-                <label htmlFor="email" className="font-medium">
-                  Email
-                </label>
-                <div className="flex items-center border border-t border-gray-300 bg-gray-50 rounded-[8px] py-1.5 px-2.5 w-full gap-3 h-11">
-                  <Mail />
-                  <input
-                    type="text"
-                    id="email"
-                    value={user.email}
-                    placeholder="Enter Your Email"
-                    className="w-full outline-none"
-                    readOnly
-                  />
-                </div>
+                <label className="font-medium">Email</label>
+                <input
+                  type="text"
+                  {...register("email", { required: "Email harus diisi" })}
+                  placeholder="Enter Your Email"
+                  className="w-full border rounded-md p-2"
+                />
+                {errors.email && (
+                  <p className="text-red-500 text-sm">{errors.email.message}</p>
+                )}
               </div>
+
+              {/* Fullname */}
               <div className="flex flex-col gap-2">
-                <label htmlFor="fullname" className="font-medium">
-                  Fullname
-                </label>
-                <div className="flex items-center border border-t border-gray-300 bg-gray-50 rounded-[8px] py-1.5 px-2.5 w-full gap-3 h-11">
-                  <UserRoundPen />
-                  <input
-                    type="text"
-                    id="fullname"
-                    value={user.fullname}
-                    placeholder="Enter Your Full Name"
-                    className="w-full outline-none"
-                    onChange={(e) => dispatch(setFullname(e.target.value))}
-                  />
-                </div>
+                <label className="font-medium">Fullname</label>
+                <input
+                  type="text"
+                  {...register("fullname", { required: "Nama harus diisi" })}
+                  placeholder="Enter Your Full Name"
+                  className="w-full border rounded-md p-2"
+                />
+                {errors.fullname && (
+                  <p className="text-red-500 text-sm">
+                    {errors.fullname.message}
+                  </p>
+                )}
               </div>
+
+              {/* Phone */}
               <div className="flex flex-col gap-2">
-                <label htmlFor="addres" className="font-medium">
-                  Address
-                </label>
-                <div className=" flex items-center border border-t border-gray-300 bg-gray-50 rounded-[8px] py-1.5 px-2.5 w-full gap-3 h-11">
-                  <MapPinCheck />
-                  <input
-                    type="text"
-                    id="address"
-                    value={user.address}
-                    placeholder="Enter Your Address"
-                    className="w-full outline-none"
-                    onChange={(e) => dispatch(setAddress(e.target.value))}
-                  />
-                </div>
+                <label className="font-medium">Phone Number</label>
+                <input
+                  type="text"
+                  {...register("phone", {
+                    required: "Nomor telepon harus diisi",
+                  })}
+                  placeholder="Enter Your Phone Number"
+                  className="w-full border rounded-md p-2"
+                />
+                {errors.phone && (
+                  <p className="text-red-500 text-sm">{errors.phone.message}</p>
+                )}
               </div>
+
+              {/* Address */}
+              <div className="flex flex-col gap-2">
+                <label className="font-medium">Address</label>
+                <input
+                  type="text"
+                  {...register("address", { required: "Alamat harus diisi" })}
+                  placeholder="Enter Your Address"
+                  className="w-full border rounded-md p-2"
+                />
+                {errors.address && (
+                  <p className="text-red-500 text-sm">
+                    {errors.address.message}
+                  </p>
+                )}
+              </div>
+
               <div className="flex flex-col gap-4">
                 <h1 className="font-medium">Delivery</h1>
 
-                <div className="flex justify-between gap-2">
-                  {["Dine In", "Door Delivery", "Pick Up"].map((option) => (
+                <div className="flex gap-2">
+                  {deliveryOptions.map((opt) => (
                     <button
-                      key={option}
+                      key={opt.id}
                       type="button"
-                      onClick={() => dispatch(setDeliveri(option))}
-                      className={`border rounded-md py-2 px-6 w-full ${
-                        product.delivery === option
-                          ? "bg-orange-400 text-white border-orange-400"
+                      onClick={() => setDelivery(opt.id)}
+                      className={`cursor-pointer hover:bg-gray-100 border rounded-md py-2 px-4 w-full ${
+                        delivery === opt.id
+                          ? "bg-[#997950] text-white"
                           : "border-gray-300"
                       }`}
                     >
-                      {option}
+                      {opt.label}
                     </button>
                   ))}
                 </div>
               </div>
-            </form>
-          </div>
-        </div>
-        <div className="my-20 lg:w-1/3 lg:my-40">
-          {/* Card Payment*/}
-          <div className="mt-10">
-            <h1 className="my-5 font-medium text-xl lg:text-2xl">Total</h1>
-            <div className="bg-gray-100 px-5 py-10 flex flex-col gap-6">
-              <div className="flex justify-between">
-                <p className="font-medium text-gray-500">Order</p>
-                <p className="font-medium">
-                  IDR {itemTotal.toLocaleString("id-ID")}
-                </p>
-              </div>
-              <div className="flex justify-between">
-                <p className="font-medium text-gray-500">Delivery</p>
-                <p className="font-medium">
-                  IDR {deliveryFee.toLocaleString("id-ID")}
-                </p>
-              </div>
-              <div className="flex justify-between">
-                <p className="font-medium text-gray-500">Tax</p>
-                <p className="font-medium">IDR {tax.toLocaleString("id-ID")}</p>
-              </div>
-              <hr className="border border-gray-300" />
-              <div className="flex justify-between">
-                <p className="font-medium text-gray-500">Sub Total</p>
-                <p className="font-medium">
-                  IDR {subTotal.toLocaleString("id-ID")}
-                </p>
-              </div>
-
               <button
-                className="bg-orange-400 w-full py-3 rounded-lg cursor-pointer"
-                onClick={handleCheckout}
+                type="submit"
+                className="cursor-pointer bg-[#997950] w-full py-3 rounded-lg mt-5 text-white"
               >
                 Checkout
               </button>
-              <div className="flex flex-col gap-5">
-                <p className="text-gray-500">We Accept</p>
-                <div className="flex justify-between gap-2">
-                  {[
-                    { name: "bri", img: "/bri.svg" },
-                    { name: "dana", img: "/dana.svg" },
-                    { name: "bca", img: "/bca.svg" },
-                    { name: "gopay", img: "/gopay.svg" },
-                    { name: "ovo", img: "/ovo.svg" },
-                    { name: "paypal", img: "/paypal.svg" },
-                  ].map((method) => (
-                    <button
-                      key={method.name}
-                      onClick={() => handlePaymentClick(method.name)}
-                      className={`p-1 rounded-md border transition ${
-                        selectedPayment === method.name
-                          ? "border-orange-500 bg-orange-100"
-                          : "border-transparent hover:border-gray-300"
-                      }`}
-                    >
-                      <img
-                        src={method.img}
-                        alt={method.name}
-                        className="h-8 w-auto"
-                      />
-                    </button>
-                  ))}
-                </div>
-                <p className="text-gray-500">
-                  *Get Discount if you pay with Bank Central Asia
-                </p>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
+
+        {/* --- CONFIRM MODAL --- */}
+        <ConfirmModal
+          isOpen={showDeleteModal}
+          title="Hapus item?"
+          message="Apakah kamu yakin ingin menghapus item ini dari keranjang?"
+          onCancel={() => setShowDeleteModal(false)}
+          onConfirm={confirmDelete}
+        />
+
+        {/* --- PAYMENT --- */}
+        <PaymentOrder
+          carts={carts}
+          selectedPayment={selectedPayment}
+          setSelectedPayment={setSelectedPayment}
+        />
       </div>
     </>
   );
