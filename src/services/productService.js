479e3501c2cd5aac53cdc39516015da6bb601e6d
federus1105 <federusrudi@gmail.com>
2025-11-23 22:31:21 +0700

    Feat ( service ) : feature function api call product admin

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,11 @@
 import { apiClient } from "./apiClient";
 
-//--- FAVORITE PRODUCT ---
+
+// ==============================
+//             USER
+// ==============================
+
+// --- FAVORITE PRODUCT ---
 export const favoriteProduct = async () => {
     return apiClient("/favorite-product", {
         method: "GET",
@@ -23,32 +28,76 @@ export const getProductById = async (productId, token) => {
   }
 }
 
-
 // --- LIST PRODUCT FILTER ---
 export const productList = async (filters) => {
    const params = new URLSearchParams();
 
-  // === FILTER NAME ===
   if (filters.name) params.append("name", filters.name);
-  
-  // === FILTER PRICE ===
   if (filters.min_price) params.append("min_price", filters.min_price);
   if (filters.max_price) params.append("max_price", filters.max_price);
-
-
-  // === CATEGORY ===
   if (filters.category && Array.isArray(filters.category)) {
     filters.category.forEach((cat) => params.append("category", cat));
   }
-
-    // === SORT BY ===
   if (filters.sortBy) params.append("sort_by", filters.sortBy);
-
-
-   // === PAGINATION ===
-  if (filters.page) params.append("page", filters.page);
+    if (filters.page) params.append("page", filters.page);
 
   return apiClient(`/product?${params.toString()}`, {
     method: "GET",
   });
+}
+
+
+// =======================================================
+//                         ADMIN
+// =======================================================
+
+export const AdminGetCategory = async (token) => {
+   return apiClient("/admin/categories", {
+        method: "GET",
+         headers: {
+         Authorization: `Bearer ${token}`,
+        }
+    })
+}
+
+export const AdminProductList = async (filters,token) => {
+     const params = new URLSearchParams();
+
+  if (filters.name) params.append("name", filters.name);
+    if (filters.page) params.append("page", filters.page);
+    return apiClient(`/admin/product?${params.toString()}`, {
+        method: "GET",
+         headers: {
+         Authorization: `Bearer ${token}`,
+        }
+    })
+}
+
+export const AdminCreateProduct = async (formData, token) => {
+  return await apiClient("/admin/product", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  });
+}
+
+export const AdminDeleteProduct = async (id_product, token) => {
+  return await apiClient(`/admin/product/delete/${id_product}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
+}
+
+export const AdminUpdateProduct = async (id_product, token, formdata) => {
+ return await apiClient(`/admin/product/${id_product}`, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formdata,
+  }); 
 }
