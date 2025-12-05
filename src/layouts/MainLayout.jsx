d37a6cfb8531e2abeb249f4d3c852e4674df5853
federus1105 <federusrudi@gmail.com>
2025-12-05 09:57:01 +0700

    Feat ( layouts ) : feature loading for main layouts

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,15 +3,25 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import { ScrollToTop } from "../routes/Routes";
+import { useState } from "react";
 
 function MainLayout() {
+    const [loading, setLoading] = useState(false);
   return (
     <div className="min-h-screen flex flex-col">
-      <ScrollToTop/>
+      <ScrollToTop />
       <Navbar />
 
       <main className="flex-grow">
-        <Outlet />
+        {loading && (
+          <div className="loader-overlay">
+            <div className="flex flex-col items-center">
+              <div className="loader"></div>
+              <span className="loader-text">Loading...</span>
+            </div>
+          </div>
+        )}
+        <Outlet context={{ setLoading }} />
       </main>
 
       <Footer />
