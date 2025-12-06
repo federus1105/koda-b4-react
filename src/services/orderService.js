9769e31843a875c0917d3ae3bf56bb0a15e29380
federus1105 <federusrudi@gmail.com>
2025-12-07 02:21:54 +0700

    Feat ( service ) : feature api call for admin pages

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,6 +1,11 @@
 import { apiClient } from "./apiClient";
 
 
+// =======================================================
+//                         USER
+// =======================================================
+
+
 // ---- CREATE CART ----
 export const createCart = async ({ product_id, quantity, size, variant, token }) => {
   const body = {
@@ -41,8 +46,8 @@ export const DeleteCart = async (id_Cart,token)=> {
   return await apiClient(`/cart/${id_Cart}`, {
     method: "DELETE",
     headers: {
-         Authorization: `Bearer ${token}`,
-        },
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
@@ -50,6 +55,45 @@ export const DeleteCart = async (id_Cart,token)=> {
 export const OrderProduct = async (input, token)=>{
   return await apiClient("/transactions", {
     method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(input),
+  });
+}
+
+
+// =======================================================
+//                         ADMIN
+// =======================================================
+
+
+export const AdminOrderList = async (filters,token) => {
+     const params = new URLSearchParams();
+
+  if (filters.ordernumber) params.append("ordernumber", filters.ordernumber);
+  if (filters.status) params.append("status", filters.status);
+    return apiClient(`/admin/order?${params.toString()}`, {
+        method: "GET",
+         headers: {
+         Authorization: `Bearer ${token}`,
+        }
+    })
+}
+
+// --- GET DETAIL ORDER ---
+export const AdminDetailOrder = async (id,token)=> {
+  return await apiClient(`/admin/order/${id}`, {
+    method: "GET",
+    headers: {
+         Authorization: `Bearer ${token}`,
+        },
+  });
+}
+
+export const AdminUpdateStatusOrder = async (input, id, token)=> {
+  return await apiClient(`/admin/order/${id}`, {
+    method: "PUT",
     headers: {
          Authorization: `Bearer ${token}`,
         },
