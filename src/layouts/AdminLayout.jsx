bd4618bd0e65cd5ae520b1af13340e14e68d17a9
federus1105 <federusrudi@gmail.com>
2025-12-06 12:05:20 +0700

    Feat ( layouts ) : feature loading for layout admin pages

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebarAdmin/Sidebar";
 import NavbarAdmin from "../components/navbarAdmin/Navbar";
 import { ScrollToTop } from "../routes/Routes";
 
 function AdminLayout() {
+  const [loading, setLoading] = useState(false);
   return (
     <>
       <div className="min-h-screen flex flex-col">
-        
-        <ScrollToTop/>
+        <ScrollToTop />
         <NavbarAdmin />
 
         <main className="min-h-screen flex">
           <Sidebar />
           <div className="w-full p-10">
-            <Outlet />
+            {loading && (
+              <div className="loader-overlay">
+                <div className="flex flex-col items-center">
+                  <div className="loader"></div>
+                  <span className="loader-text">Loading...</span>
+                </div>
+              </div>
+            )}
+            <Outlet context={{ setLoading }} />
           </div>
         </main>
       </div>
